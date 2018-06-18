61ddabcdf090298bb8137abb7fd1fd1496af0822
Kevin-Kip <masterfork5@gmail.com>
2018-06-18 13:02:39 +0300

    Updates to the layout

--- app/src/main/java/com/truekenyan/orderfood/InfoActivity.java
+++ app/src/main/java/com/truekenyan/orderfood/InfoActivity.java
@@ -1,7 +1,11 @@
 package com.truekenyan.orderfood;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick (R.id.button_buy)
+    public void onViewClicked () {
+        startActivity(new Intent(getApplicationContext(), BookingActivity.class));
     }
 }
--- app/src/main/java/com/truekenyan/orderfood/MainActivity.java
+++ app/src/main/java/com/truekenyan/orderfood/MainActivity.java
@@ -1,7 +1,11 @@
 package com.truekenyan.orderfood;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick (R.id.button_view)
+    public void onViewClicked () {
+        startActivity(new Intent(getApplicationContext(), InfoActivity.class));
+//        TODO add more functionality and animation
     }
 }
