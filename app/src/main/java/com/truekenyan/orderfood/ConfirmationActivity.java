707172585fc2cfc863415dd9c1237c213c5accd2
Kevin-Kip <masterfork5@gmail.com>
2018-06-18 16:10:51 +0300

    Updates to the layout

--- app/src/main/java/com/truekenyan/orderfood/ConfirmationActivity.java
+++ app/src/main/java/com/truekenyan/orderfood/ConfirmationActivity.java
@@ -1,13 +1,25 @@
 package com.truekenyan.orderfood;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ConfirmationActivity extends AppCompatActivity {
+
+    @BindView (R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
