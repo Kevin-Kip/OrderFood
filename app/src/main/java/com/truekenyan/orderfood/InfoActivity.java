477087ee29ddb27a916390c9f8a6d52b442b514d
Kevin-Kip <masterfork5@gmail.com>
2018-06-19 12:39:16 +0300

    Updates to the layout logic

--- app/src/main/java/com/truekenyan/orderfood/InfoActivity.java
+++ app/src/main/java/com/truekenyan/orderfood/InfoActivity.java
@@ -1,6 +1,8 @@
 package com.truekenyan.orderfood;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
